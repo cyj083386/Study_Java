dd6d25a5abfd10a4f089caae45414810673e60ae
cyj <cyj083386@gmail.com>
2023-09-26 17:57:55 +0900

    후위,전위 연산자 Scanner 사용자 입력

---
+++ 0926/JHOper.java
@@ -0,0 +1,26 @@
+package com.mystudy.run.etc;
+
+public class JHOper {
+    public static void main(String[] args) {
+        int a =5;
+        int sum = 0;
+
+        System.out.println("a= "+a+" sum= "+sum);
+
+        sum = a++ + ++a;
+
+        System.out.println("a= "+a+" sum= "+sum);
+
+        sum = 0;
+        sum += a++; //sum = sum + ++a
+        //열이 다르므로
+        sum += ++a;
+
+        System.out.println("a= "+a+" sum= "+sum);
+
+
+        for (int i = 0; i < 10 ; i++) {
+            System.out.print(i+1+" ");
+        }
+    }
+}
